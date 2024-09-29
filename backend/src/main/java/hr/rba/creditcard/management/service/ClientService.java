3b20489cb9c993473f22e0af63655e2931b82d3a
Amir Muhadri <amir.muhadri@intellexi.hr>
2024-09-29 23:43:00 +0200

    feat: implement get all clients in repository, service, and controller + application.properties - add h2 db properties

--- backend/src/main/java/hr/rba/creditcard/management/service/ClientService.java
+++ backend/src/main/java/hr/rba/creditcard/management/service/ClientService.java
@@ -1,7 +1,20 @@
 package hr.rba.creditcard.management.service;
 
+import hr.rba.creditcard.management.entity.Client;
+import hr.rba.creditcard.management.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public List<Client> findAllClients() {
+        return clientRepository.findAll();
+    }
+
 }
