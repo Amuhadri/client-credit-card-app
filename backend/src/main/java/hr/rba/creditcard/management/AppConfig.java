78aee70761052f38e5acde556e3b077dea10ece0
Amir Muhadri <amir.muhadri@intellexi.hr>
2024-09-30 00:36:05 +0200

    feat: implement send client data to API by OIB

---
+++ backend/src/main/java/hr/rba/creditcard/management/AppConfig.java
@@ -0,0 +1,14 @@
+package hr.rba.creditcard.management;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
