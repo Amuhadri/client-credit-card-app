bfb5fbdabbf2e30f3ce67f4d2235b119bb5cea51
Amir Muhadri <amir.muhadri@intellexi.hr>
2024-09-29 23:50:14 +0200

    chore: add logging to get client by OIB functionality

--- backend/src/main/java/hr/rba/creditcard/management/controller/ClientController.java
+++ backend/src/main/java/hr/rba/creditcard/management/controller/ClientController.java
@@ -2,6 +2,8 @@ package hr.rba.creditcard.management.controller;
 
 import hr.rba.creditcard.management.entity.Client;
 import hr.rba.creditcard.management.service.ClientService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +17,16 @@ import java.util.List;
 @RequestMapping("/api/clients")
 public class ClientController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ClientController.class);
+
     @Autowired
     private ClientService clientService;
 
     @GetMapping
     public ResponseEntity<List<Client>> getAllClients() {
+        logger.info("Received request to get all clients");
         List<Client> clients = clientService.findAllClients();
+        logger.info("Found {} clients", clients.size());
         return new ResponseEntity<>(clients, HttpStatus.OK);
     }
 }
