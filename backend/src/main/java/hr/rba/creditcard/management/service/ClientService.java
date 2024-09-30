32683731c0bc4c518c81c505b65bd042330532f1
Amir Muhadri <amir.muhadri@intellexi.hr>
2024-09-30 14:50:52 +0200

    chore: apply general updates and improvements

--- backend/src/main/java/hr/rba/creditcard/management/service/ClientService.java
+++ backend/src/main/java/hr/rba/creditcard/management/service/ClientService.java
@@ -2,10 +2,7 @@ package hr.rba.creditcard.management.service;
 
 import hr.rba.creditcard.management.entity.Client;
 import hr.rba.creditcard.management.repository.ClientRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,20 +11,12 @@ import java.util.Optional;
 @Service
 public class ClientService {
 
-    private static final Logger logger = LoggerFactory.getLogger(ClientService.class);
-
     @Autowired
     private ClientRepository clientRepository;
 
-    @Autowired
-    private ExternalApiService externalApiService;
-
     public List<Client> findAllClients() {return clientRepository.findAll();}
 
-    public Optional<Client> findClientByOib(String oib) {
-        return clientRepository.findByOib(oib);
-    }
-
+    public Optional<Client> findClientByOib(String oib) {return clientRepository.findByOib(oib);}
     public Client saveClient(Client client) {
         return clientRepository.save(client);
     }
@@ -49,22 +38,5 @@ public class ClientService {
         Optional<Client> client = clientRepository.findByOib(oib);
         client.ifPresent(clientRepository::delete);
     }
-
-    public String sendClientData(String oib) {
-        Optional<Client> client = findClientByOib(oib);
-        if (client.isPresent()) {
-            // Client found, try sending data to external API
-            ResponseEntity<String> apiResponse = externalApiService.sendClientData(client.get());
-
-            if (apiResponse.getStatusCode().is2xxSuccessful()) {
-                return "Client found and successfully sent: " + client.get().toString() + "\nAPI Response: " + apiResponse.getBody();
-            } else {
-                return "Client found, but the API is unavailable or returned an error: " + apiResponse.getStatusCode() + " - " + apiResponse.getBody();
-            }
-        } else {
-            throw new RuntimeException("Client not found");
-        }
-    }
-
 
 }
